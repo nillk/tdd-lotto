217d03c454e7e5120d77220d02e2ef0c6f1ea3e9
nillk <hyunjoo88.kim@gmail.com>
2019-05-20 01:13:31 +0900

    입력 금액은 1000원 이상이어야 한다

--- src/main/java/LottoGame.java
+++ src/main/java/LottoGame.java
@@ -1,12 +1,16 @@
 
 public class LottoGame {
 
-    private static final int PRICE = 1000;
+    private static final int PRICE_BASE = 1000;
 
     private int lotto = 0;
 
-    public int buy(int i) {
-        lotto = i / PRICE;
+    public int buy(int price) {
+        if (price < PRICE_BASE) {
+            throw new RuntimeException("구매 금액은 1000원 이상이어야 합니다.");
+        }
+
+        lotto = price / PRICE_BASE;
         return lotto;
     }
 }
--- src/test/java/LottoGameTest.java
+++ src/test/java/LottoGameTest.java
@@ -1,14 +1,24 @@
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class LottoGameTest {
 
     @Test
     public void 천원당_한_장의_로또를_구매할_수_있다() {
         LottoGame lottoGame = new LottoGame();
-        int count = lottoGame.buy(2000);
 
-        assertThat(count).isEqualTo(2);
+        assertThat(lottoGame.buy(1000)).isEqualTo(1);
+        assertThat(lottoGame.buy(2000)).isEqualTo(2);
+    }
+
+    @Test
+    public void 구입_금액은_천원_이상이어야_한다() {
+        LottoGame lottoGame = new LottoGame();
+
+        assertThatExceptionOfType(RuntimeException.class).
+                isThrownBy(() -> lottoGame.buy(600)).
+                withMessage("구매 금액은 1000원 이상이어야 합니다.");
     }
 }
