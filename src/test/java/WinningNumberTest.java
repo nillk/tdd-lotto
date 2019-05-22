36eb8087a1908a48d7c64215c862da6ac7b0bee9
nin-pride <nin.pride@kakaocorp.com>
2019-05-22 21:00:18 +0900

    보너스 숫자가 1에서 45사이가 아니면 에러 발생

--- src/test/java/WinningNumberTest.java
+++ src/test/java/WinningNumberTest.java
@@ -1,8 +1,7 @@
 import lotto.Lotto;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class WinningNumberTest {
 
@@ -39,7 +38,22 @@ public class WinningNumberTest {
 
     @Test
     public void 보너스_번호는_하나의_숫자를_입력해야_한다() {
+        try {
+            new WinningNumber("1,2,3,4,5,6", "1");
+        } catch (Exception e) {
+            fail("에러가 발생하지 않아야 함");
+        }
+
         assertThatThrownBy(() -> new WinningNumber("1,2,3,4,5,6", "1,2"))
                 .hasMessage("보너스 번호는 하나의 숫자여야 합니다.");
+    }
+
+    @Test
+    public void 보너스_번호가_1에서_45_사이의_숫자가_아니면_에러_발생() {
+        assertThatThrownBy(() -> new WinningNumber("1,2,3,4,5,6", "0"))
+                .hasMessage("숫자는 1보다 크거나 같아야 합니다.");
+
+        assertThatThrownBy(() -> new WinningNumber("1,2,3,4,5,6", "46"))
+                .hasMessage("숫자는 45보다 작거나 같아야 합니다.");
     }
 }
